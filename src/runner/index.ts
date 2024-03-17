4c7a1870cceb8e1dcc6481c60a85387b9153f925
JothamWong <45916998+JothamWong@users.noreply.github.com>
2024-03-18 00:05:12 +0800

    Chore: Remove files for a clean slate

--- src/runner/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './fullJSRunner'
-export * from './htmlRunner'
-export * from './sourceRunner'
-export * from './utils'
--- src/utils/astToString.ts
+++
@@ -1,4 +0,0 @@
-import { generate } from 'astring'
-import { Node } from 'estree'
-
-export const astToString = (node: Node): string => generate(node)
