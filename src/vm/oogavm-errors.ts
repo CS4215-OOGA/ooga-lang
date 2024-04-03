09f214877e8aeb9397e4a250fb1110316656bf7f
JothamWong <45916998+JothamWong@users.noreply.github.com>
2024-04-03 12:43:09 +0800

    dev: format oogavm-errors.ts

--- src/vm/oogavm-errors.ts
+++ src/vm/oogavm-errors.ts
@@ -1,30 +1,28 @@
 // Collection of ooga errors to distinguish ooga-lang errors from typescript errors.
 export class OogaError extends Error {
-  constructor(msg: string) {
-    super(msg);
-  }
+    constructor(msg: string) {
+        super(msg);
+    }
 }
 
-export class ParseError extends OogaError {
-
-}
+export class ParseError extends OogaError {}
 
 export class HeapError extends OogaError {
-  constructor(msg: string) {
-    super(msg);
-  }
+    constructor(msg: string) {
+        super(msg);
+    }
 }
 
 // HeapOutOfMemoryError is an error that might be fixable.
 export class HeapOutOfMemoryError extends OogaError {
-  constructor() {
-    super("Out of memory!");
-  }
+    constructor() {
+        super('Out of memory!');
+    }
 }
 
 // No more memory and cannot save. OOGA!
 export class HeapDeadError extends OogaError {
-  constructor() {
-    super("Out of memory!");
-  }
+    constructor() {
+        super('Out of memory!');
+    }
 }
